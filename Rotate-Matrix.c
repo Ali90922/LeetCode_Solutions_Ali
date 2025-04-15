5e91a7b38e0556c08e67f9b0325381902db25e59
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2025-04-15 12:29:03 -0500

    Problem Solved

--- Rotate-Matrix.c
+++ Rotate-Matrix.c
@@ -8,9 +8,27 @@ void rotate(int** matrix, int matrixSize, int* matrixColSize) {
 //Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 
 
-Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
-Output: [[7,4,1],[8,5,2],[9,6,3]]
 
+// Cannot create a new matrix -- To get it done first have to transponse - and reverse the rows !
+
+// Step 1: Transpose the matrix
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = i + 1; j < matrixSize; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    // Step 2: Reverse each row
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < matrixSize / 2; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][matrixSize - 1 - j];
+            matrix[i][matrixSize - 1 - j] = temp;
+        }
+    }
+}
 
     
-}
+
